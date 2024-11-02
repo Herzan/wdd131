dcc7c20e8355d6520a04d52c6e79df27559951fe
Herzan <46695573+Herzan@users.noreply.github.com>
2024-11-01 22:36:32 -0600

    f

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -1,9 +1,9 @@
 // Get the current year for the copyright
-document.getElementById('currentyear').textContent = new Date().getFullYear();
+document.getElementById('current-year').textContent = new Date().getFullYear();
 
 // Get the last modified date and time
 const lastModified = new Date(document.lastModified);
 const formattedDate = lastModified.toLocaleString(); // Converts to local date and time string
 
 // Display the last modified date and time in the footer
-document.getElementById('lastModified').textContent = `Last Modified: ${formattedDate}`;
+document.getElementById('last-modified').textContent = formattedDate; // Corrected ID here
